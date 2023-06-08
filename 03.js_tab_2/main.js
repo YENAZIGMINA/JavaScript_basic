3e87f9ade97b41436f54c5d644f95a54f5797fb5
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-06-08 18:52:31 +0900

    Add files via upload

--- 03.js_tab_2/main.js
+++ 03.js_tab_2/main.js
@@ -10,7 +10,18 @@ function openCity (evt,cityName){
         tablinks[i].classList.remove('active');
     }
     //this = currentTarget 비슷함 - 후자는 클릭한 요소를 가져옴
-    console.log(evt.currentTarget) //currentTarget index에 작성한 태그
+    console.log(evt.currentTarget) 
+    //currentTarget -> index에 작성한 태그를 콘솔에 찍음
     evt.currentTarget.classList.add('active');
+    document.getElementById(cityName).style.display="block";
 }
 
+document.getElementById('defaultOpen').click();
+
+//🎀classList 메서드
+//.classList.add('active') --> active라는 클래스를 추가
+//.classList.remove('active') --> active라는 클래스를 제거
+//.classList.toggle('active') --> active 클래스가 존재하면 active클래스를 제거하고
+//                              active 클래스가 존재하지 않으면 active클래스를 추가
+//.classList.contain('active') --> active클래스가 존재하는가? (true/false로 반환)
+//.classList.replace('A','B) --> A클래스를 B클래스로 변경
