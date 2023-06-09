42b3c1a2e639775beb9e17a9d2e3c59e8968b173
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-06-09 18:16:40 +0900

    Add files via upload

---
+++ 07.js_tab_3/main.js
@@ -0,0 +1,17 @@
+function openCity(cityName, elem, color){
+    let tabcontent = document.getElementsByClassName('tabcontent');
+    for(let i=0; i<tabcontent.length; i++){
+        tabcontent[i].style.display="none";
+        //console.log(tabcontent[i]) //i는 지역변수
+    }
+
+    let tablinks = document.getElementsByClassName('tablink');
+    for(let i=0; i<tablinks.length; i++){
+        tablinks[i].style.backgroundColor="";
+    }
+    elem.style.backgroundColor=color;
+    document.getElementById(cityName).style.display="block";
+
+}
+
+document.getElementById('defaultOpen').click();
