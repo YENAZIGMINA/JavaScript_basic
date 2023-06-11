cb8ea005270e706620e206b31d0cc28ca4208fad
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-06-11 15:55:04 +0900

    Update main.js

--- 07.js_tab_3/main.js
+++ 07.js_tab_3/main.js
@@ -8,6 +8,7 @@ function openCity(cityName, elem, color){
     let tablinks = document.getElementsByClassName('tablink');
     for(let i=0; i<tablinks.length; i++){
         tablinks[i].style.backgroundColor="";
+        //❗빈 문자열을 할당하면, backgroundColor 속성은 기본값으로 초기화됨
     }
     elem.style.backgroundColor=color;
     document.getElementById(cityName).style.display="block";
