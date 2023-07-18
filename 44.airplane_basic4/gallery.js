4b1f48d90fa704a9a3ec3a0e39d5f3d512de39ab
YENAZIGMINA <129706758+YENAZIGMINA@users.noreply.github.com>
2023-07-18 17:52:45 +0900

    Add files via upload

---
+++ 44.airplane_basic4/gallery.js
@@ -0,0 +1,16 @@
+let galleryTimeline=document.querySelector('.gallery-timeline');
+let gallery=document.querySelector('.gallery');
+
+
+gallery.animate([
+    {transform:'translateX(0)'},
+    {transform:'translateX(calc(-100% + 100vw))'}
+],{
+    fill: 'both',
+        timeline: new ScrollTimeline({
+            scrollOffsets: [
+                {target:galleryTimeline,edge:'start',threshold:1},
+                {target:galleryTimeline,edge:'end',threshold:1}
+            ]
+        })
+})
